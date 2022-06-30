c7e2bc8e8db0d9bf85acd402e96c5bbe096eccdd
Daniil Vodolazsky <daniil.vodolazsky@mail.ru>
2022-07-01 00:13:52 +0300

    Updated init

--- data_readers/__init__.py
+++ data_readers/__init__.py
@@ -1,9 +1,19 @@
-from .abstract_readers import ReaderAbstract, AnalysesReaderAbstract
+from .abstract_readers import (
+    ReaderAbstract,
+    AnalysesReaderAbstract
+)
 from .aucopro_reader import AuCoProReader
 from .char_deps_reader import CharDepsReader
 from .demonext_reader import DemonextReader
+from .derivatario_reader import DerivaTarioReader
 from .germanet_reader import GermaNetReader
+from .morphynet_reader import MorphyNetDerivationalReader
 from .uder_reader import UDerReader
+from .wfl_readers import (
+    WordFormationLatinReaderAbstract,
+    WordFormationLatinSQLReader,
+    WordFormationLatinXMLReader
+)
 
 __all__ = [
     "ReaderAbstract",
@@ -11,6 +21,11 @@ __all__ = [
     "AuCoProReader",
     "CharDepsReader",
     "DemonextReader",
+    "DerivaTarioReader",
     "GermaNetReader",
-    "UDerReader"
+    "MorphyNetDerivationalReader",
+    "UDerReader",
+    "WordFormationLatinReaderAbstract",
+    "WordFormationLatinXMLReader",
+    "WordFormationLatinSQLReader"
 ]
